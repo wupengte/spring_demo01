64809c60e563bdc4679ced920fbe1925a49b183d
wpt <421486096@qq.com>
2020-09-11 16:26:05 +0800

    2020-09-11 v01

---
+++ src/main/java/com/mgspring/book_xml/Test.java
@@ -0,0 +1,23 @@
+package com.mgspring.book_xml;
+
+import com.mgspring.book_xml.controller.BookController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 功能描述
+ * Project: spring_demo01
+ * Package: com.mgspring.book
+ * Version: 1.0
+ * Created by WUPENGTE on 2020/9/10 15:28
+ * Description:TODO
+ * Modify: TODO
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        ApplicationContext cap = new ClassPathXmlApplicationContext("META-INF/book_xml.xml");
+        BookController bookController = cap.getBean("bookController", BookController.class);
+        bookController.buyBook();
+    }
+}
---
+++ src/main/java/com/mgspring/book_xml/serivce/impl/BookServiceImpl.java
@@ -0,0 +1,31 @@
+package com.mgspring.book_xml.serivce.impl;
+
+import com.mgspring.book_xml.dao.BookDao;
+import com.mgspring.book_xml.serivce.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 功能描述
+ * Project: spring_demo01
+ * Package: com.mgspring.book.serivce.impl
+ * Version: 1.0
+ * Created by WUPENGTE on 2020/9/10 15:13
+ * Description:TODO
+ * Modify: TODO
+ */
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao dao;
+
+    @Override
+//    @Transactional
+    public void buyBook(String bid, String uid) {
+        Integer price = dao.selectPrice(bid);
+        dao.updateSt(bid);
+        dao.updateBalance(uid, price);
+    }
+}
